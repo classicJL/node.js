2125e57c10d6228ee27c7ef2d42aef21958e6551
HyeonjunLee <hyeonjun217.lee@gmail.com>
2024-02-02 14:09:48 +0900

    node.js with javascript

---
+++ web_server/main.js
@@ -0,0 +1,12 @@
+const express = require('express') //npm install express --save
+
+const app = express()
+
+app.listen(3000, ()=>{
+    console.log('Listen!!')
+})
+
+// add routines
+app.get()
+
+app.get()
